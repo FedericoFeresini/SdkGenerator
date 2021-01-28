0f82d860d926ff589ce942f0b34a48e81e76f427
Federico Alberto Livio Feresini <fal.feresini@criteo.com>
2021-01-28 15:08:46 +0100

    move file

---
+++ .github/actions/generateSDK.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ -d ./out ]]
+then 
+	rm -Rf ./out
+fi
+
+mkdir ./out
+
+#docker run --rm --name devtest -v "//c/dev/generator/out://local" openapitools/openapi-generator-cli generate \
+#    -i https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml \
+#    -g csharp \
+#    -o //local
+
+#docker run --rm --name devtest -v "/${PWD}://local" openapitools/openapi-generator-cli generate \
+#    -i //local/mySwagger.json \
+#    -g csharp \
+#    -o //local/out
+
