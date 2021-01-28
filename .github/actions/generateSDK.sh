e1c8aa15bfffe6986e416c464414c04afb3e9623
FedericoFeresini <65897118+FedericoFeresini@users.noreply.github.com>
2021-01-28 19:49:16 +0100

    Update generateSDK.sh

--- .github/actions/generateSDK.sh
+++ .github/actions/generateSDK.sh
@@ -6,13 +6,15 @@ fi
 
 mkdir ./out
 
+echo "created out directory"
+
 #docker run --rm --name devtest -v "//c/dev/generator/out://local" openapitools/openapi-generator-cli generate \
 #    -i https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml \
 #    -g csharp \
 #    -o //local
 
-docker run --rm --name devtest -v "/${PWD}://local" openapitools/openapi-generator-cli generate \
-    -i //local/mySwagger.json \
-    -g csharp \
-    -o //local/out
+#docker run --rm --name devtest -v "/${PWD}://local" openapitools/openapi-generator-cli generate \
+#    -i //local/mySwagger.json \
+#    -g csharp \
+#    -o //local/out
 
