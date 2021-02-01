09614acc1c79a7bf73906644c37dbbe9eede2323
FedericoFeresini <65897118+FedericoFeresini@users.noreply.github.com>
2021-02-01 19:53:16 +0100

    Update entrypoint.sh

--- .github/mustache/entrypoint.sh
+++ .github/mustache/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cat << /app/properties
+cat /app/properties
 echo "==================="
-cat << /app/properties mo /app/application.properties.template > /app/application.properties
+cat /app/properties mo /app/application.properties.template > /app/application.properties
 echo "-------------------------"
-cat << /app/application.properties
+cat /app/application.properties
 exec "$@"
