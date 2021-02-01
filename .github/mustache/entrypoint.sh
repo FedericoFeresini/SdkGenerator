1fbe55adab954443b102bb3561273c658cf68b6a
FedericoFeresini <65897118+FedericoFeresini@users.noreply.github.com>
2021-02-01 19:30:59 +0100

    Update entrypoint.sh

--- .github/mustache/entrypoint.sh
+++ .github/mustache/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cat << /app/properties | mo /app/application.properties.template 
+cat << /app/properties mo /app/application.properties.template 
 #> /app/application.properties
 echo "-------------------------"
+NAME=ico SURNAME=fere mo /app/application.properties.template 
 #cat << /app/application.properties
 exec "$@"
